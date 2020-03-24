d8624c1b955c6d9d23c2d5fcd43e1ddff15a80a5
Vo Ho Viet Khoa <vhvkhoa@gmail.com>
2020-03-24 12:18:08 +0900

    First commit

--- sort.py
+++ sort.py
@@ -17,16 +17,19 @@
 """
 from __future__ import print_function
 
-from numba import jit
+import glob
+import time
+import argparse
 import os.path
+import pickle as pkl
+
+from numba import jit
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from skimage import io
 from sklearn.utils.linear_assignment_ import linear_assignment
-import glob
-import time
-import argparse
+
 from filterpy.kalman import KalmanFilter
 
 @jit
@@ -171,7 +174,6 @@ def associate_detections_to_trackers(detections,trackers,iou_threshold = 0.3):
   return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
 
 
-
 class Sort(object):
   def __init__(self,max_age=1,min_hits=3):
     """
@@ -232,67 +234,60 @@ class Sort(object):
 def parse_args():
     """Parse input arguments."""
     parser = argparse.ArgumentParser(description='SORT demo')
+    parser.add_argument('--input_video', default='cam_1', help='Path to directory containing video frames.')
+    parser.add_argument('--input_bboxes', default='bboxes/cam_1.mp4.pkl', help='Path to pkl file containing video bboxes.')
     parser.add_argument('--display', dest='display', help='Display online tracker output (slow) [False]',action='store_true')
     args = parser.parse_args()
     return args
 
 if __name__ == '__main__':
   # all train
-  sequences = ['PETS09-S2L1','TUD-Campus','TUD-Stadtmitte','ETH-Bahnhof','ETH-Sunnyday','ETH-Pedcross2','KITTI-13','KITTI-17','ADL-Rundle-6','ADL-Rundle-8','Venice-2']
   args = parse_args()
+  video_path = args.input_video
+  bboxes_path = args.input_bboxes
   display = args.display
-  phase = 'train'
+
   total_time = 0.0
   total_frames = 0
   colours = np.random.rand(32,3) #used only for display
+
   if(display):
-    if not os.path.exists('mot_benchmark'):
-      print('\n\tERROR: mot_benchmark link not found!\n\n    Create a symbolic link to the MOT benchmark\n    (https://motchallenge.net/data/2D_MOT_2015/#download). E.g.:\n\n    $ ln -s /path/to/MOT2015_challenge/2DMOT2015 mot_benchmark\n\n')
-      exit()
     plt.ion()
     fig = plt.figure() 
+
+  with open(bboxes_path, 'rb') as f:
+    seq_dets = pkl.load(f)
   
-  if not os.path.exists('output'):
-    os.makedirs('output')
-  
-  for seq in sequences:
-    mot_tracker = Sort() #create instance of the SORT tracker
-    seq_dets = np.loadtxt('data/%s/det.txt'%(seq),delimiter=',') #load detections
-    with open('output/%s.txt'%(seq),'w') as out_file:
-      print("Processing %s."%(seq))
-      for frame in range(int(seq_dets[:,0].max())):
-        frame += 1 #detection and frame numbers begin at 1
-        dets = seq_dets[seq_dets[:,0]==frame,2:7]
-        dets[:,2:4] += dets[:,0:2] #convert to [x1,y1,w,h] to [x1,y1,x2,y2]
-        total_frames += 1
+  mot_tracker = Sort() #create instance of the SORT tracker
+  for frame in range(len(seq_dets)):
+    dets = np.concatenate([seq_dets[frame][3], seq_dets[frame][8]], axis=0)
+    total_frames += 1
 
-        if(display):
-          ax1 = fig.add_subplot(111, aspect='equal')
-          fn = 'mot_benchmark/%s/%s/img1/%06d.jpg'%(phase,seq,frame)
-          im =io.imread(fn)
-          ax1.imshow(im)
-          plt.title(seq+' Tracked Targets')
+    if(display):
+      ax1 = fig.add_subplot(111, aspect='equal')
+      fn = '%s/%06d.jpg'%(video_path, frame + 1)
+      im =io.imread(fn)
+      ax1.imshow(im)
+      plt.title('Tracked Targets')
 
-        start_time = time.time()
-        trackers = mot_tracker.update(dets)
-        cycle_time = time.time() - start_time
-        total_time += cycle_time
+    start_time = time.time()
+    trackers = mot_tracker.update(dets)
+    cycle_time = time.time() - start_time
+    total_time += cycle_time
 
-        for d in trackers:
-          print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame,d[4],d[0],d[1],d[2]-d[0],d[3]-d[1]),file=out_file)
-          if(display):
-            d = d.astype(np.int32)
-            ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
-            ax1.set_adjustable('box-forced')
+    for d in trackers:
+      print('%d,%d,%.2f,%.2f,%.2f,%.2f,1,-1,-1,-1'%(frame, d[4], d[0], d[1], d[2] - d[0], d[3] - d[1]))
+      if(display):
+        d = d.astype(np.int32)
+        ax1.add_patch(patches.Rectangle((d[0],d[1]),d[2]-d[0],d[3]-d[1],fill=False,lw=3,ec=colours[d[4]%32,:]))
+        ax1.set_adjustable('box')
 
-        if(display):
-          fig.canvas.flush_events()
-          plt.draw()
-          ax1.cla()
+    if(display):
+      fig.canvas.flush_events()
+      plt.draw()
+      ax1.cla()
 
   print("Total Tracking took: %.3f for %d frames or %.1f FPS"%(total_time,total_frames,total_frames/total_time))
   if(display):
     print("Note: to get real runtime results run without the option: --display")
   
-
-
