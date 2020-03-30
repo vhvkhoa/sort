7a6cfbc1fa3e013379751960b71a99e84f9d4639
Vo Ho Viet Khoa <vhvkhoa@Vos-MacBook-Pro.local>
2020-03-30 19:55:50 +0700

    update

--- goturn.py
+++ goturn.py
@@ -1,3 +1,5 @@
+from os import path
+from glob import glob
 import argparse
 import pickle as pkl
 
@@ -10,9 +12,14 @@ import pycocotools.mask as mask_util
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '--input-video',
+        '--input-video-dir',
         type=str,
-        default='AIC20_track1/Dataset_A/cam_1.mp4'
+        default='AIC20_track1/Dataset_A/'
+    )
+    parser.add_argurment(
+        'video-extension',
+        type=str,
+        default='mp4'
     )
     parser.add_argument(
         '--input-bbox',
@@ -54,74 +61,77 @@ def draw_bbox(frame, bbox, bbox_id):
 
 
 def main(args):
-    with open(args.input_bbox, 'rb') as f:
-        bboxes = pkl.load(f)
+    input_video_paths = glob(path.join(args.input_video_dir, '*.' + args.video_extension))
 
-    input_video = cv2.VideoCapture(args.input_video)
-    width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    output_video = cv2.VideoWriter(
-        filename=args.output_video,
-        fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
-        fps=30.,
-        frameSize=(width, height),
-        isColor=True
-    )
-    num_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
+    for input_video_path in input_video_paths:
+        with open(path.join(args.input_bbox_dir, input_video_path + '.pkl'), 'rb') as f:
+            bboxes = pkl.load(f)
 
-    trackers = []
-    tracked_bboxes = []
-    bbox_ids = []
-    current_bbox_id = 0
-
-    for frame_idx in tqdm(range(num_frames)):
-        success, frame = input_video.read()
-
-        # Keep cars and trucks
-        frame_bboxes = np.concatenate([
-            bboxes[frame_idx][3],
-            bboxes[frame_idx][8]],
-            axis=0
+        input_video = cv2.VideoCapture(input_video_path)
+        width = int(input_video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(input_video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        output_video = cv2.VideoWriter(
+            filename=args.output_video,
+            fourcc=cv2.VideoWriter_fourcc(*'mp4v'),
+            fps=30.,
+            frameSize=(width, height),
+            isColor=True
         )
+        num_frames = int(input_video.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        # Keep bboxes with confidence score more than threshold
-        frame_bboxes = [
-            bbox[:4].astype(np.int32) for bbox in frame_bboxes
-            if bbox[4] > args.confidence_thresh
-        ]
+        trackers = []
+        tracked_bboxes = []
+        bbox_ids = []
+        current_bbox_id = 0
 
-        # Remove bboxes that cannot be tracked or exists over a threshold
-        untracked_ids = []
-        for i, tracker in enumerate(trackers):
-            success, bbox = tracker.update(frame)
-            if success:  # and frame_idx - start_frame < args.time_thresh:
-                tracked_bboxes[i] = np.array([bbox[0], bbox[1], bbox[2] + bbox[0], bbox[3] + bbox[1]])
-            else:
-                untracked_ids.append(i)
-        if len(untracked_ids) > 0:
-            for index in untracked_ids[::-1]:
-                del tracked_bboxes[index]
-                del trackers[index]
-                del bbox_ids[index]
+        for frame_idx in tqdm(range(num_frames)):
+            success, frame = input_video.read()
 
-        if len(frame_bboxes) > 0 and len(tracked_bboxes) > 0:
-            ious = mask_util.iou(np.array(frame_bboxes), np.array(tracked_bboxes), np.zeros((len(tracked_bboxes),), dtype=np.bool))
-        elif len(frame_bboxes) > 0:
-            ious = np.zeros((len(frame_bboxes), 1))
+            # Keep cars and trucks
+            frame_bboxes = np.concatenate([
+                bboxes[frame_idx][3],
+                bboxes[frame_idx][8]],
+                axis=0
+            )
 
-        max_iou_per_new = np.asarray(ious).max(axis=1).tolist()
-        for iou, bbox in zip(max_iou_per_new, frame_bboxes):
-            if iou <= args.iou_thresh:
-                tracked_bboxes.append(bbox)
-                bbox_ids.append(current_bbox_id)
-                trackers.append(cv2.TrackerMOSSE_create())
-                bbox = (bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1])
-                trackers[-1].init(frame, bbox)
-                current_bbox_id += 1
+            # Keep bboxes with confidence score more than threshold
+            frame_bboxes = [
+                bbox[:4].astype(np.int32) for bbox in frame_bboxes
+                if bbox[4] > args.confidence_thresh
+            ]
 
-        for bbox, bbox_id in zip(tracked_bboxes, bbox_ids):
-            frame = draw_bbox(frame, bbox, bbox_id)
-        output_video.write(frame)
+            # Remove bboxes that cannot be tracked or exists over a threshold
+            untracked_ids = []
+            for i, tracker in enumerate(trackers):
+                success, bbox = tracker.update(frame)
+                if success:  # and frame_idx - start_frame < args.time_thresh:
+                    tracked_bboxes[i] = np.array([bbox[0], bbox[1], bbox[2] + bbox[0], bbox[3] + bbox[1]])
+                else:
+                    untracked_ids.append(i)
+            if len(untracked_ids) > 0:
+                for index in untracked_ids[::-1]:
+                    del tracked_bboxes[index]
+                    del trackers[index]
+                    del bbox_ids[index]
+
+            if len(frame_bboxes) > 0 and len(tracked_bboxes) > 0:
+                ious = mask_util.iou(np.array(frame_bboxes), np.array(tracked_bboxes), np.zeros((len(tracked_bboxes),), dtype=np.bool))
+            elif len(frame_bboxes) > 0:
+                ious = np.zeros((len(frame_bboxes), 1))
+
+            max_iou_per_new = np.asarray(ious).max(axis=1).tolist()
+            for iou, bbox in zip(max_iou_per_new, frame_bboxes):
+                if iou <= args.iou_thresh:
+                    tracked_bboxes.append(bbox)
+                    bbox_ids.append(current_bbox_id)
+                    trackers.append(cv2.TrackerMOSSE_create())
+                    bbox = (bbox[0], bbox[1], bbox[2] - bbox[0], bbox[3] - bbox[1])
+                    trackers[-1].init(frame, bbox)
+                    current_bbox_id += 1
+
+            for bbox, bbox_id in zip(tracked_bboxes, bbox_ids):
+                frame = draw_bbox(frame, bbox, bbox_id)
+            output_video.write(frame)
 
 
 if __name__ == '__main__':
